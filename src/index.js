fb3122d6349e217a9ef7459b2ed7a08ca489c6b3
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-25 15:12:29 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './config/connectDB.js';
+
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = 4000;
 
 
 app.use(cors());
+app.use(express.json());
+connectDB();
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
