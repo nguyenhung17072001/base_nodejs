625f627b0d64d36beabc73966dea0cc15b2a96d0
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-25 13:48:56 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
-const app = express()
-const port = 3000
+import cors from 'cors';
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
-})
+});
